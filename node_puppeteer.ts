28cfad1f663a2c8d87f0c9f4876a2c873bf32cde
tigershark2020 <64547173+tigershark2020@users.noreply.github.com>
2023-12-20 23:09:50 -0600

    Update node_puppeteer.ts

--- node_puppeteer.ts
+++ node_puppeteer.ts
@@ -1,16 +1,20 @@
 const express = require('express')
 const replace = require('absolutify')
-// const puppeteer = require('puppeteer-extra');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+// const puppeteer = require('puppeteer-extra-plugin-recaptcha');
+// const puppeteer = require('puppeteer');
+var sleep = require('sleep'); 
+
 const port = 1337;
 
 const app = express()
 
 app.get('/', async (req, res) => {
 
-    const { url } = req.query
-    
-    if (!url) {
+    const url = req.query.url
+    const time_delay = req.query.time_delay
+
+    if (url == null || url == '') {
         var urlNotSetStatus = "URL Not Set";
         return res.send(urlNotSetStatus);
     } else {
@@ -18,40 +22,67 @@ app.get('/', async (req, res) => {
 		const buff = Buffer.from(url, "base64");
 		const urlBase64Decoded = buff.toString("utf8");
 		console.log(urlBase64Decoded);
-		// const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-		// puppeteer.use(StealthPlugin());
+		const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+		puppeteer.use(StealthPlugin());
 
-
-		puppeteer.launch({headless: false, ignoreHTTPSErrors: true, executablePath: '/usr/bin/google-chrome', ignoreHTTPSErrors: true, defaultViewport: null, args: ["--ignore-certificate-errors"]}).then(async browser => {
+		puppeteer.launch({headless: false, ignoreHTTPSErrors: true, args: [`--window-size=1920,1849`,"--ignore-certificate-errors"], executablePath: '/usr/bin/google-chrome', ignoreHTTPSErrors: true, defaultViewport: {width: 1920, height: 1849}}).then(async browser => {
 		
 			try
 			{			
 				const page = await browser.newPage();
-				
-				await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.101 Safari/537.36");
-				
-				await page.goto(urlBase64Decoded, {waitUntil: 'load'});
 
+
+				await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+				
+				const response = await page.goto(urlBase64Decoded, {waitUntil: 'load'});
+				const headers = response.headers();
+				
+				let content_type = "";
+				
+				if("content-type" in headers)
+				{
+					content_type = headers["content-type"];
+				}
+
+				// await page.solveRecaptchas();
 				// await autoScroll(page);		
 									
-				let document = await page.evaluate(() => document.documentElement.outerHTML);
-
-				document = replace(document, urlBase64Decoded);
-
-				let base64Encoded = Buffer.from(document).toString("base64");
-				console.log("Returning response to client");
-				
-									
-				page.on('requestfailed', request => {
-					console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`);
-
+				await page.on('requestfailed', request => {
+					if(request.failure() != null)
+					{
+						if(request.failure().errorText != null)
+						{
+							console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`);
+						}
+					}
 				});
 				
-				page.on('unhandledRejection', (reason, p) => {
+				await page.on('unhandledRejection', (reason, p) => {
 				  	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 				});
-						
-						
+				
+				const cookies = await page.cookies()
+
+				await sleep.sleep(parseInt(time_delay))
+
+				// let base64Encoded = Buffer.from(document).toString("base64");
+
+
+				const imageBuffer = await page.screenshot({
+							type: 'png'
+			    	});
+			    	
+				let screenshotBase64String = Buffer.from(imageBuffer).toString("base64");
+				let document = await page.evaluate(() => document.documentElement.outerHTML);
+				document = replace(document, urlBase64Decoded);
+				
+				// console.log("Response Status:\t" + response.status());
+				let jsonObject = { "HTML" : document, "Screenshot_PNG" :  screenshotBase64String, "Content_Type" : content_type, "Status_Code" : response.status()}
+				let responseJSON = JSON.stringify(jsonObject);
+				// console.log(responseJSON);
+				let base64Encoded = Buffer.from(responseJSON).toString("base64");
+				
+				console.log("Returning response to client");	
 				return res.send(base64Encoded);
 			
 			} catch (err) {
@@ -92,6 +123,6 @@ async function autoScroll(page){
     });
 }
 
-app.listen(port, '192.168.1.43', function () {
+app.listen(port, '192.168.1.161', function () {
     console.log('Listening to port:  ' + port);
 });
